8bfaa1788465bd39cdac06588e0086d45ab292ea
malodetz <malo.detz@yandex.ru>
2021-10-20 23:19:15 +0300

    fixes for test one 2

--- fine-tuning-experiments/test_model.py
+++ fine-tuning-experiments/test_model.py
@@ -123,6 +123,9 @@ def calculate_metrics(run_id: str, snapshot_iteration: str, partition: str = "te
     bleus = []
     chrfs = []
     for p, t in zip(predictions, targets):
+        if p == "" or t == "":
+            bleus.append(0)
+            chrfs.append(0)
         bleus.append(calc_bleu(p, t))
         chrfs.append(calc_chrf(p, t))
 
